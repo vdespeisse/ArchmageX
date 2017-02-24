0e117fa69b914eed6b35e7cafc54f1e11ea354d3
vdespeisse <v.despeisse@gmail.com>
2017-02-24 23:44:58 +0100

    fqs

---
+++ Assets/Scripts/Abilities/Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+	Vector3 currentPosition;
+
+	public float range = 10f;
+
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		//if(Input.GetKey(KeyCode.S))   
+	}
+
+
+		
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private float speed = 10;
+
+	private Vector3 targetPosition;
+	private bool isMoving;
+
+
+
+	void Start () {
+
+		targetPosition = transform.position;
+		isMoving = false;	
+	}
+
+
+	void Update () {
+		if(Input.GetMouseButton(1))
+			setTargetPosition();
+
+		if(isMoving)
+			Move();
+	}
+
+	void setTargetPosition(){
+		Plane plane = new Plane(Vector3.up, transform.position);
+		Ray ray =  Camera.main.ScreenPointToRay(Input.mousePosition);
+		float point = 0f;
+
+		if(plane.Raycast(ray, out point))
+			targetPosition = ray.GetPoint(point);
+
+		isMoving = true;
+	}
+	void Move(){
+		transform.LookAt(targetPosition);
+		transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+		if(transform.position == targetPosition)
+			isMoving = false;
+		Debug.DrawLine(transform.position, targetPosition, Color.red);
+ 	 }
+
+
+}
