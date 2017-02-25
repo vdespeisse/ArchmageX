53bb93c6b9c85918acbf9cf75614ba141efc5c03
firekoala <skyz3ra@gmail.com>
2017-02-25 18:28:03 +0100

    shields

---
+++ Assets/Scripts/Abilities/Shield.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour {
+
+	public float duration;
+	float timer;
+	public GameObject shield;
+
+
+	void Start () {
+		timer = duration;
+	}
+
+	void Update () {
+		timer -= Time.deltaTime;
+
+
+		if(Input.GetMouseButton(2))
+		   ShieldActivate();
+
+	}
+
+	void ShieldActivate() {
+		GameObject s;
+		s = (GameObject)Instantiate (shield, transform.position, shield.transform.rotation);
+	
+	if (timer <= 0) 
+		Destroy(s);
+
+	}
+
+	public void Death(){
+	}
+}
