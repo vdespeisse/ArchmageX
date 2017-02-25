404f489941d7f9e87d6c4505ad9dd7a8568a3863
firekoala <skyz3ra@gmail.com>
2017-02-25 17:11:02 +0100

    hook

---
+++ Assets/Scripts/Abilities/GrapplingHook.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingHook : MonoBehaviour {
+
+
+	Vector3 targetPosition;
+	public bool isFlying = false;
+	public float speed = 15f;
+
+
+
+	void Start () {
+	}
+	
+	void Update () {
+		if(Input.GetKey(KeyCode.A)) {
+			setTargetPosition();
+			isFlying = true;
+			}
+
+
+		if(isFlying) {
+			transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed *Time.deltaTime);
+			if(transform.position == targetPosition)
+				isFlying =false;
+		}
+
+		
+	}
+
+
+	void setTargetPosition(){
+		Plane plane = new Plane(Vector3.up, transform.position);
+		Ray ray =  Camera.main.ScreenPointToRay(Input.mousePosition);
+		float point = 0f;
+		
+		if(plane.Raycast(ray, out point))
+			targetPosition = ray.GetPoint(point);
+	}
+}
