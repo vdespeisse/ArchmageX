8dc2916858dda0ffe62e9e62a005059fe70b6fad
firekoala <skyz3ra@gmail.com>
2017-02-27 04:02:10 +0100

    voila

--- Assets/Scripts/Abilities/Abilities.cs
+++ Assets/Scripts/Abilities/Abilities.cs
@@ -6,6 +6,7 @@ public class Abilities : MonoBehaviour {
 
 	public GameObject shield;
 
+
 	void Start () {
 		
 	}
--- Assets/Scripts/Hero/HealthBar.cs
+++ Assets/Scripts/Hero/HealthBar.cs
@@ -5,34 +5,62 @@ using UnityEngine.UI;
 
 
 public class HealthBar : MonoBehaviour {
+	
 
 
-	public Image currentHealthbar;
-	public Text ratioText;
-	public GameObject sd;
-
+	private Image currentHealthbar;
+	private Text ratioText;
+	private GameObject g;
+		
 	private float hitpoint  =150f;
 	private float maxHitpoint = 150f;
-
+	
 	private void Start() {
 
-		UpdateHealthbar();
+		GameObject g = GameObject.FindGameObjectWithTag("HPImage");
+		GameObject t = GameObject.FindGameObjectWithTag("textImage");
+
+		if(g != null)
+		{
+			currentHealthbar = g.GetComponent<Image>();
+		}
+
+		if(t != null)
+		{
+			ratioText = t.GetComponent<Text>();
+		}
+
+
 	}
-
+	
 	private void UpdateHealthbar() {
-
+		
 		float ratio = hitpoint / maxHitpoint;
 		currentHealthbar.rectTransform.localScale = new Vector3(ratio,1,1);
-		ratioText.text = (ratio*100).ToString()  + '%';
+		ratioText.text = (ratio*100).ToString("0")  + '%';
 	}
-
+	
 	private void TakeDamage(float damage) {
-
+		
+		hitpoint -= damage;
+		if(hitpoint < 0)
+		{
+			hitpoint = 0;
+			Debug.Log ("Dead!");
+		}
+		UpdateHealthbar();
 
 	}
-
+	
 	private void HealDamage(float heal) {
-
+		
+		hitpoint += heal;
+		if(hitpoint > maxHitpoint)
+		{
+			hitpoint = maxHitpoint;
+		}
+		UpdateHealthbar();
 
 	}
+
 }
